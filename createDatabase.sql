6847417023f3d10d9b4f040ab5f8b4c7a2d11a73
BenLink1 <benjamin.link@centre.edu>
2023-11-03 00:35:04 +0000

    finished build script

--- createDatabase.sql
+++ createDatabase.sql
@@ -13,3 +13,29 @@ CREATE TABLE specimens (
     specimenId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     isAlive BOOLEAN,
 );
+CREATE TABLE specimens (
+    speciesName VARCHAR(64) FOREIGN KEY,
+    specimenId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    isAlive BOOLEAN,
+    photograph VARCHAR(64)
+);
+CREATE TABLE clients (
+    clientAddress VARCHAR(64),
+    clientAreaCode VARCHAR(64),
+    clientCity VARCHAR(64),
+    clientCountry VARCHAR(64),
+    clientEmail VARCHAR(64),
+    clientID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    clientPhone VARCHAR(15),
+    clientProfession VARCHAR(64),
+    clientState VARCHAR(64)
+)
+
+CREATE TABLE requests (
+    clientID INT FOREIGN KEY,
+    specimenID INT FOREIGN KEY,
+    requestDate DATE,
+    dateOut DATE,
+    dateIn Date,
+    requestID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+)
