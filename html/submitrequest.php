716faef9bed5b20c2eb0897a4fa547628357827a
noahring <noah.ring@centre.edu>
2023-11-10 19:27:18 +0000

    More edits to request form. CSS styling generated by ChatGPT

--- html/submitrequest.php
+++ html/submitrequest.php
@@ -1,3 +1,29 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    $config = parse_ini_file('/home/noahring/mysql.ini');
+    $conn = new mysqli(
+        $config['mysqli.default_host'],
+        $config['mysqli.default_user'],
+        $config['mysqli.default_pw']);
+
+    if ($conn->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+        exit();
+    }
+
+    $conn->select_db('bugs');
+
+    $query = "SELECT * FROM species";
+    $result = $conn->query($query);
+
+    $result_body = $result->fetch_all();
+    $num_rows = $result->num_rows;
+    $num_cols = $result->field_count;
+    $fields = $result->fetch_fields();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +39,13 @@
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 100vh;
+            min-height: 100vh;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
         }
 
         form {
@@ -22,59 +54,68 @@
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             width: 300px;
+            margin-bottom: 20px;
         }
 
-        label {
-            display: block;
-            margin-bottom: 8px;
-            font-weight: bold;
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            margin-top: 20px;
         }
 
-        input {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 16px;
-            box-sizing: border-box;
+        table, th, td {
+            border: 1px solid #ddd;
         }
 
-        input[type="submit"] {
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
             background-color: #4caf50;
-            color: #fff;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-
-        button {
-            background-color: #ccc;
-            color: #333;
-            padding: 8px;
-            cursor: pointer;
-            border: none;
-            border-radius: 4px;
-        }
-
-        button:hover {
-            background-color: #ddd;
+            color: white;
         }
     </style>
 </head>
 <body>
 
-<form action="process_request.php" method="post">
-    <label for="species">Species Name:</label>
-    <input type="text" id="species" name="species" required>
+<div class="container">
+    <form action="process_request.php" method="post">
+        <label for="species">Species Name:</label>
+        <input type="text" id="species" name="species" required>
 
-    <label for="quantity">Quantity:</label>
-    <input type="number" id="quantity" name="quantity" required>
+        <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" required>
 
-    <div style="display: flex; justify-content: space-between; margin-top: 16px;">
-        <input type="submit" value="Submit">
-        <a href="index.php"><button type="button">Cancel</button></a>
-    </div>
-</form>
+        <div style="display: flex; justify-content: space-between; margin-top: 16px;">
+            <input type="submit" value="Submit">
+            <a href="index.php"><button type="button">Cancel</button></a>
+        </div>
+    </form>
+    <h2>Available Species</h2>
+    <table>
+    <thead>
+        <tr>
+            <?php foreach ($fields as $field): ?>
+                <th><?php echo $field->name; ?></th>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach ($result_body as $row): ?>
+            <tr>
+                <?php foreach ($row as $cell): ?>
+                    <td><?php echo $cell; ?></td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+</div>
 
 </body>
 </html>
+
