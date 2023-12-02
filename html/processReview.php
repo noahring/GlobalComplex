aabb5e2e34452abd03c98b529c27dd95bbe6a4a5
BenLink1 <benjamin.link@centre.edu>
2023-12-02 21:32:59 +0000

    one last thing

--- html/processReview.php
+++ html/processReview.php
@@ -20,7 +20,6 @@ of the desired species -->
      }
  
      $conn->select_db('bugs');
-
      
     
     foreach($_POST as $key => $value){
@@ -43,14 +42,17 @@ of the desired species -->
 
 
     $result = $conn->query($query);
+    $unixtime = time();
+    //echo $unixtime;
     for($i=0; $i< $numRows; $i++){
         //echo "reached";
          $id = $data[$i][0];
-         //var_dump($_POST);
+         //Svar_dump($_POST);
          foreach($_POST as $key => $value){
-            if($key=="date".$id){
+            if($key=="send".$id){
+                echo "reached";
                 $preparedUpdateStatement = "UPDATE transactions
-                                               SET dateOut = (?)
+                                               SET dateOut = (FROM_UNIXTIME({$unixtime}))
                                              WHERE transactionID =(?)";
                 $updateStatementObject=$conn->prepare($preparedUpdateStatement);
                 $updateStatementObject->bind_param("i", $id);
@@ -58,8 +60,20 @@ of the desired species -->
             }
         }
     }
-
+    ?><h1>Unsent Transactions<h2>
+    <?php
     resultToHTMLTable($result);
+
+    $query = "SELECT transactionID, clientFirstName, clientLastName, speciesName, dateOut
+                    FROM transactions 
+                     INNER JOIN clients
+                        ON transactions.clientID = clients.clientID
+                     INNER JOIN specimens 
+                        ON specimens.specimenID = transactions.specimenID
+                     WHERE transactions.dateOut IS NOT NULL;";
+    $result = $conn->query($query);
+    resultToHTMLTable($result);
+
     
 ?>
 </html>
